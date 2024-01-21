75d555d0495d4d8f436b6c50247240a7cc008916
Aapo <aapo.jutila@aalto.fi>
2024-01-21 22:36:26 +0200

    added static middleware

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ morgan.token("payload", function(req, res) { return JSON.stringify(req.body) })
 const app = express()
 
 app.use(cors())
-// app.use(express.static("dist"))
+app.use(express.static("dist"))
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :payload"))
 
